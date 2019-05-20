5ed046022e9f7f364ea0bac57bc5960e35b31433
Emmanuel Tierra <emman.tierra@gmail.com>
2019-05-20 14:27:37 +0800

    Create base API controller

---
+++ application/modules/api/controllers/Api.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+require(APPPATH . '/libraries/REST_Controller.php');
+
+class Api extends REST_Controller
+{
+    public function __construct(){
+        parent::__construct();
+    }
+}
