84504139a016302f7ed161fac6b05982c40dcfba
Emmanuel Tierra <emman.tierra@gmail.com>
2019-05-20 14:28:03 +0800

    Create API respond method

--- application/modules/api/controllers/Api.php
+++ application/modules/api/controllers/Api.php
@@ -7,4 +7,25 @@ class Api extends REST_Controller
     public function __construct(){
         parent::__construct();
     }
+
+    public function respond($data)
+    {
+        $status = $data['status'] ?? 200;
+
+        $response = [
+            'status' => $status,
+            'success' => $status >= 200 || $status <= 299,
+            'message' => $data['message'] ?? '',
+            'data' => $data['data'] ?? [],
+        ];
+
+        if (!$response['success']) {
+            $response['error'] = [
+                'message' => $response['message'],
+            ];
+            unset($response['message']);
+        }
+
+        $this->response($response, $status);
+    }
 }
